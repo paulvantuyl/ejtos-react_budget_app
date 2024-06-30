df63222706395d765de11ba8be2083897d0dbab4
Paul <paulvantuyl@users.noreply.github.com>
2024-06-30 16:57:56 -0700

    Add/create the ExpenseTotal.js component

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,17 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext);
+    // Use reduce function to get a total of all the costs, assigning this to a var and displaying the va in the JSX
+    // Whenever the user adds an expense, this causes the state to update, which will cause all components connected to the context to re-render and update themselves with new values.
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: £{totalExpenses}</span>
+        </div>
+    );
+};
+export default ExpenseTotal;
