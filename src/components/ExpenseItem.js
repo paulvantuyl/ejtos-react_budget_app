505c751b232274e92cc1165f7962f2190b94e4d0
Paul <paulvantuyl@users.noreply.github.com>
2024-07-04 07:04:19 -0700

    Update expenselist functions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { TiDelete, TiPlus } from 'react-icons/ti';
+import { PlusCircleFill, DashCircleFill, XCircleFill } from 'react-bootstrap-icons';
 import { AppContext } from '../context/AppContext';
 
 // Dispatching an action that contains the type, so the reducer knows how to update the state and the payload
@@ -26,15 +26,42 @@ const ExpenseItem = (props) => {
         });
     };
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense,
+        });
+    };
+
     return (
         <tr>
             <td>{props.name}</td>
             <td>{Currency}{props.cost}</td>
             <td>
-                <TiPlus size="1.5em" onClick={event => increaseAllocation(props.name)}></TiPlus>
+                <PlusCircleFill 
+                    color="#198754" 
+                    size="1.5em"
+                    onClick={(event) => increaseAllocation(props.name)}
+                />
             </td>
             <td>
-                <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
+                <DashCircleFill
+                    color="#ffc106"
+                    size="1.5em"
+                    onClick={(event) => decreaseAllocation(props.name)}
+                />
+            </td>
+            <td>
+                <XCircleFill
+                    color="#dc3445"
+                    size="1.5em"
+                    onClick={handleDeleteExpense}
+                />
             </td>
         </tr>
     );
