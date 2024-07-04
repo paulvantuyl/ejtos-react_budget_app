6151baf7b7f04fe75062428725d6201fc2ed0dbd
Paul <paulvantuyl@users.noreply.github.com>
2024-07-04 05:24:07 -0700

    Cleanup commented out code

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -25,44 +25,6 @@ const Currency = () => {
                 <option value="₹">₹ Ruppee</option>
             </select>
         </div>
-        
-        // <div className="dropdown">
-        //     <button
-        //         className="btn btn-secondary dropdown-toggle"
-        //         type="button"
-        //         data-bs-toggle="dropdown"
-        //         aria-expanded="false"
-        //         id="Currency"
-        //         onChange={event => changeCurrency(event.target.value)}
-        //     >
-        //         Currency ({Currency})
-        //     </button>
-
-
-
-        //     <ul className="dropdown-menu">
-        //         <li>
-        //             <a className="dropdown-item" value="$">
-        //                 $ Dollar
-        //             </a>
-        //         </li>
-        //         <li>
-        //             <a className="dropdown-item" value="£">
-        //                 £ Pound
-        //             </a>
-        //         </li>
-        //         <li>
-        //             <a className="dropdown-item" value="€">
-        //                 € Euro
-        //             </a>
-        //         </li>
-        //         <li>
-        //             <a className="dropdown-item" value="₹">
-        //                 ₹ Ruppee
-        //             </a>
-        //         </li>
-        //     </ul>
-        // </div>
     );
 };
 
